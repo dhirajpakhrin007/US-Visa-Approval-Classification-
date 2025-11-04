1ce0d905b7203e3f74ce99beab0995c2e23e3e4b
dhirajpakhrin007 <dhirajpakhrin95@gmail.com>
2025-11-04 12:15:07 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setuptools(
+    name='us_visa',
+    version='0.0.0',
+    author='Dhiraj',
+    author_email='dhirajpakhrin95@gmail.com',
+    packages=find_packages()
+)
